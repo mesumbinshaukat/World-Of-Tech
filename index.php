325d799f2321237c7426e3b1f2f292bc429882bc
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2023-04-03 17:00:01 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>WORLD OF TECH</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="./bootstrap-5.2.3/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <section id="navbar">
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <div>
+                    <a class="navbar-brand" href="#">WORLD OF TECH</a>
+                </div>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Contact Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Services</a>
+                        </li>
+
+
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+
+    </section>
+
+    <section>
+        <main>
+            <div class="container-fluid mt-5">
+                <center>
+                    <div class="row mt-5 text-center justify-content-center">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-12 mt-5 text-center">
+                            <h1 class="text-center mt-5">COMING SOON!!!</h1>
+                        </div>
+
+                    </div>
+                </center>
+            </div>
+        </main>
+    </section>
+
+    <section>
+        <footer>
+            <div class="container-fluid bg-dark footer  ">
+                <div class="row justify-content-center ">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-12 text-center justify-content-center mt-3">
+                        <p class="text-light">DEVELOPED BY <b>MESUM BIN SHAUKAT</b></p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </section>
+</body>
+
+</html>
