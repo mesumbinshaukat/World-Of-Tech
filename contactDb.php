edbd546307ec8b49b64bc46d5d236c5e334ec1f2
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2023-04-04 22:22:18 +0500

    Updated. 4/4/2023 10:23pm by Mesum

--- contactDb.php
+++ contactDb.php
@@ -1,5 +1,6 @@
 <?php 
-$conn = mysqli_connect('localhost', 'root', 'XcRny943ve76JB', 'world_of_tech') or die("Can't Connect");
+// $conn = mysqli_connect('localhost', 'root', 'XcRny943ve76JB', 'world_of_tech') or die("Can't Connect");
+$conn = mysqli_connect('localhost', 'root', '', 'world_of_tech') or die("Can't Connect");
 
 if ($conn) {
     $submit_btn = $_POST['btn_submit'];
@@ -11,9 +12,20 @@ if ($conn) {
         $subject = $_POST['subject'];
         $message = $_POST['message'];
 
-        $insert_query = "INSERT INTO `user_contact_details`(`user_name`, `user_email`, `phone_number`, `subject`, `message`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5])";
+        $insert_query = "INSERT INTO `user_contact_details`(`user_name`, `user_email`, `phone_number`, `subject`, `message`) VALUES ('$user_name','$user_email','$phone_number','$subject','$message')";
+
+        $run_insert_query = mysqli_query($conn, $insert_query);
+
+        if ($run_insert_query) {
+            echo "<script>alert('Form submitted successfuly')</script>";
+            header('location:index.php');
+            exit();
+        }
     }else {
-        die("Could not connect to database" . mysqli_connect_error());
+        $err = die("Could not connect to database: " . mysqli_connect_error());
+        echo "<script>alert('$err')</script>";
+        header('location:contact.php');
+        exit();
     }
 }
 
