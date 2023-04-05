d097c113a1fabca2b58b8de6aa533ad03a8c5481
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2023-04-06 00:21:40 +0500

    Updated

--- about.php
+++ about.php
@@ -8,18 +8,38 @@
     <link href="./bootstrap-5.2.3/css/bootstrap.min.css" rel="stylesheet">
     <link href="./css/style.css" rel="stylesheet">
     <style>
-        .breadcrumb a {
-    color: #6c757d;}
+    .breadcrumb a {
+        color: #6c757d;
+    }
+
     .breadcrumb {
-    padding: 30px 5px 26px;
-    background-color: transparent;
-    margin-bottom: 0;
-    border-radius: 0;
-    border-bottom: 1px solid #ddd
-}
+        padding: 30px 5px 26px;
+        background-color: transparent;
+        margin-bottom: 0;
+        border-radius: 0;
+        border-bottom: 1px solid #ddd
+    }
 
+    .about_pic {
+        height: 30rem;
+        border-radius: 20px;
 
+    }
 
+    .icon {
+        /* height: 50%; */
+        width: 30%;
+    }
+
+    .t_img {
+        height: 60%;
+        width: 50%;
+    }
+
+    .t_imgd {
+        height: 50%;
+        width: 30%;
+    }
     </style>
 
 </head>
@@ -35,48 +55,182 @@
         <div class="row">
 
 
+            <div class="inner-page-wrapper">
+
+
+
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <div class="breadcrumb-nav">
+                                <nav aria-label="breadcrumb">
+                                    <ol class="breadcrumb">
+
+                                        <li class="home breadcrumb-item"><a href="index.php">Home</a></li>
+                                        <li class="post post-page current-item breadcrumb-item active"><a
+                                                href="about.php"><b>About</b></a></li>
+                                    </ol>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container mt-4">
+                    <div class="row">
+                        <h3 class="text-center">
+                            <b>About Us..</b>
+                        </h3>
+                        <p class="text-center mt-3">
+                            World of tech is a company that provides specialization primarily in software products, in
+                            either business
+                            or consumer software based on the needs of their client base and structure for a software
+                            release.
+                        </p>
+                        <img src="./images/about-us-pic.jpg" alt="code" class="about_pic mt-4">
+                    </div>
+                </div>
+
+
+
+                <div class="container mt-5">
+                    <div class="row">
+                        <h3 class="text-center">
+                            <b>What Makes Us Diffrent..</b>
+                        </h3>
+                        <p class="text-center mt-3">
+                            We've established this company because it deals with all sorts of technologies like Web/App
+                            development, Digital marketing,
+                            SEO, etc. We can help everyone to implement these technologies in their Factories, Offices,
+                            Shops, etc. So, in this way, everyone
+                            will get a chance to compete with their competitors.You can also hire us to keep your
+                            website up to date.
+                        </p>
+                        <div class="container mt-5">
+                            <div class="row">
+                                <div class="col text-center">
+                                    <img src="./icons/web-dev.jpg" alt="" class="icon">
+                                    <p class="mt-3">Web development</p>
+                                </div>
+                                <div class="col text-center">
+                                    <img src="./Icons/app-development.png" alt="" class="icon">
+                                    <p class="mt-3">App development</p>
+                                </div>
+                                <div class="col text-center">
+                                    <img src="./Icons/designer.png" alt="" class="icon">
+                                    <p class="mt-3">UI/UX Designing</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="container mt-3">
+                            <div class="row">
+                                <div class="col text-center">
+                                    <img src="./icons/digital.png" alt="" class="icon">
+                                    <p class="mt-3">Digital Marketing</p>
+                                </div>
+                                <div class="col text-center">
+                                    <img src="./Icons/database-storage.png" alt="" class="icon">
+                                    <p class="mt-3">Database Designing</p>
+                                </div>
+                                <div class="col text-center">
+                                    <img src="./Icons/seo.png" alt="" class="icon">
+                                    <p class="mt-3">SEO</p>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+
+
+
+
+
+
+                    </div>
+                </div>
+
+
+                <div class="container mt-5">
+                    <div class="row">
+                        <h3 class="text-center"><b>Meet Our Executive Team</b> </h3>
+                        <p class="text-center mt-3">
+                            This team operates as an interdependent, collaborative decision-making body – strategically
+                            executing the company's mission and visionWorld of tech is a company that provides
+                            specialization primarily in software products, in either business
+                            or consumer software based on the needs of their client base and structure for a software
+                            release.
+                        </p>
+                        <div class="container mt-5">
+                            <div class="row">
+                                <div class="col text-center">
+                                    <img src="./Icons/anony2.jpg" alt="" class="t_img">
+                                    <h6>Mesum</h6>
+                                    <p class="mt-3">Developer</p>
+                                </div>
+                                <div class="col text-center">
+                                    <img src="./Icons/anony2.jpg" alt="" class="t_img">
+                                    <h6>Abdul Rafay</h6>
+                                    <p class="mt-3">Developer</p>
+                                </div>
+                                <div class="col text-center">
+                                    <img src="./Icons/anony2.jpg" alt="" class="t_img">
+                                    <h6>Zohair</h6>
+                                    <p class="mt-3">Developer</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="container mt-3">
+                            <div class="row">
+                                <div class="col text-center">
+                                    <img src="./Icons/anony2.jpg" alt="" class="t_img">
+                                    <h6>Sarim</h6>
+                                    <p class="mt-3">Developer</p>
+                                </div>
+                                <div class="col text-center">
+                                    <img src="./Icons/anony2.jpg" alt="" class="t_img">
+                                    <h6>Huzaifa</h6>
+                                    <p class="mt-3">Developer</p>
+                                </div>
+                                <div class="col text-center">
+                                    <img src="./Icons/anony2.jpg" alt="" class="t_img">
+                                    <h6>Mashood</h6>
+                                    <p class="mt-3">Developer</p>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+                <div class="container mt-3">
+                    <div class="row">
+                        <div class="col text-center">
+                            <img src="./Icons/anony2.jpg" alt="" class="t_imgd">
+                            <h6>Umer</h6>
+                            <p class="mt-3">Developer</p>
+                        </div>
+                        <div class="col text-center">
+                            <img src="./Icons/anony2.jpg" alt="" class="t_imgd">
+                            <h6>Owais</h6>
+                            <p class="mt-3">Developer</p>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
         </div>
+
     </div>
-    <div class="inner-page-wrapper">	
-
-	
-
-
-	<div class="container">
-		<div class="row">						
-			<div class="col">
-				<div class="breadcrumb-nav">
-					<nav aria-label="breadcrumb">
-					  	<ol class="breadcrumb">
-					    	
-<li class="home breadcrumb-item"><a href="index.php">Home</a></li>
-<li class="post post-page current-item breadcrumb-item active"><a href="about.php"><b>About</b></a></li>
-					  	</ol>
-					</nav>
-				</div>
-			</div>
-		</div>
-	</div>	 
-
-    <div class="container mt-4">
-        <div class="row">
-            <h3 class="text-center">
-              <b>Get more productivity with us..</b> 
-            </h3>
-            
-
-
-
-        </div>
     </div>
 
 
 
-    
-    
-    <section id="footer">
-        <?php include("footer.php");?>
-    </section>
+
+
 </body>
 
 </html>
