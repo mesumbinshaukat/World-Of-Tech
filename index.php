ed5014eabae7cb08ab058dba826b82d5f3a49aad
Huzaifa Irfan <huzaifairfan2144@gmail.com>
2023-05-04 00:09:12 +0500

    tfggtfht
    hthth

--- index.php
+++ index.php
@@ -91,41 +91,41 @@
 
 
 
-    <section id="carousel">
 
-        <?php include('carousel.html'); ?>
-    </section>
+
     <br><br>
+
     <section>
         <div class="container-fluid ">
             <?php include('startingcards.html'); ?>
         </div>
-        <section>
-            <br><br>
-            <section class="">
-                <div class="container-fluid ">
-                    <?php include('secondcard.html'); ?>
-                </div>
-                <section id='plans'>
-                    <?php include('plans.php'); ?>
+    <section>
 
-                    <section>
-                        <footer class="footer mt-5">
-                            <?php include("footer.html"); ?>
-                        </footer>
-                    </section>
+    <br><br>
+
+    <section class="">
+        <div class="container-fluid ">
+            <?php include('secondcard.html'); ?>
+        </div>
+    </section>
 
 
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
-                        integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
-                        crossorigin="anonymous"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-                        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-                        crossorigin="anonymous">
-                    </script>
-                    <script src="https://code.jquery.com/jquery-3.6.4.min.js"
-                        integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous">
-                    </script>
+    
+    <section>
+        <footer class="footer mt-5">
+            <?php include("footer.html"); ?>
+        </footer>
+    </section>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+
+
                     <script>
                     $(document).ready(function() {
                         $(window).scroll(function() {
